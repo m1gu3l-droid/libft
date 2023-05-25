a64a6ea05c0f1a7fb3f12d7426481659d890b569
Fabio Miguel Novais Amorim <fnovais-@student.42porto.com>
2023-05-25 01:10:17 +0100

    new_libft

--- Makefile
+++ Makefile
@@ -22,10 +22,13 @@ SRC = ft_atoi.c ft_bzero.c ft_calloc.c ft_isalnum.c ft_isalpha.c ft_isascii.c \
 	 ft_isdigit.c ft_isprint.c ft_itoa.c ft_memchr.c ft_memcmp.c ft_memcpy.c ft_memmove.c \
 	 ft_memset.c ft_putchar_fd.c ft_putendl_fd.c ft_putnbr_fd.c ft_putstr_fd.c ft_split.c \
 	 ft_strchr.c ft_strdup.c ft_strjoin.c ft_strlcat.c ft_strlcpy.c ft_strlen.c ft_strmapi.c \
-	 ft_strncmp.c ft_strnstr.c ft_strrchr.c ft_striteri.c ft_strtrim.c ft_substr.c ft_tolower.c ft_toupper.c
+	 ft_strncmp.c ft_strnstr.c ft_strrchr.c ft_striteri.c ft_strtrim.c ft_substr.c ft_tolower.c ft_toupper.c \
+	 ft_printf.c ft_putchar.c ft_putnbr.c ft_putnbr_lowhexa.c ft_putnbr_unsigned.c ft_putnbr_upperhexa.c \
+	 ft_putptr.c ft_putstr.c ft_itoa_printf.c \
 
 BONUS = ft_lstnew.c ft_lstadd_front.c ft_lstsize.c ft_lstlast.c ft_lstadd_back.c ft_lstdelone.c \
-		ft_lstclear.c ft_lstiter.c ft_lstmap.c
+		ft_lstclear.c ft_lstiter.c ft_lstmap.c \
+
 
 BONUS_OBJ = $(BONUS:.c=.o)
 
--- ft_itoa_printf.c
+++ ft_itoa_printf.c
@@ -6,13 +6,13 @@
 /*   By: fnovais- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/05 22:45:29 by fnovais-          #+#    #+#             */
-/*   Updated: 2022/12/14 17:44:36 by fnovais-         ###   ########.fr       */
+/*   Updated: 2022/12/07 15:30:56 by fnovais-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	size_of_int(int n)
+int	size_of_int(int n)
 {
 	int	i;
 
@@ -27,7 +27,7 @@ static int	size_of_int(int n)
 	return (i);
 }
 
-char	*ft_itoa(int n)
+int	ft_itoa_printf(int n, int size)
 {
 	int		count;
 	int		sign;
@@ -37,7 +37,7 @@ char	*ft_itoa(int n)
 	count = size_of_int(n);
 	s = malloc((sizeof(char) * (count + 1)));
 	if (!s)
-		return (NULL);
+		return (0);
 	s[count] = '\0';
 	if (n < 0)
 	{
@@ -48,16 +48,10 @@ char	*ft_itoa(int n)
 		s[0] = '0';
 	while (n != 0)
 	{
-		count--;
-		s[count] = (n % 10 * sign) + '0';
+		s[--count] = (n % 10 * sign) + '0';
 		n = n / 10;
 	}
-	return (s);
+	size += ft_putstr(s);
+	free(s);
+	return (size);
 }
-/*
-int	main(void)
-{
-		printf("%s\n", ft_itoa(0));
-		return (0);
-}
-*/
